17e8027b73ebd9c9e98d241eaeb59a0399e0b375
tino <tkoumapl@email.essex.edu>
2026-06-13 10:26:04 -0400

    adding 2a_emotion_detection.txt

---
+++ emotion_detection.py
@@ -0,0 +1,20 @@
+import requests
+
+def emotion_detector(text_to_analyze):
+
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+
+    payload = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    # return raw response text (as required by assignment)
+    return response.text
