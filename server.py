38883355d99f6f3b3ea6c49bdc2d624aac33d76d
tino <tkoumapl@email.essex.edu>
2026-06-13 12:48:09 -0400

    adding task 6 and its result

---
+++ server.py
@@ -0,0 +1,39 @@
+
+from flask import Flask, request, jsonify, render_template
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask(__name__)
+
+@app.route("/emotionDetector")
+def emotion_detector_route():
+
+    text_to_analyze = request.args.get("textToAnalyze")
+
+    if not text_to_analyze:
+        return jsonify({"error": "Invalid input"}), 400
+
+    result = emotion_detector(text_to_analyze)
+
+    response_str = (
+        f"For the given statement, the system response is "
+        f"'anger': {result['anger']}, "
+        f"'disgust': {result['disgust']}, "
+        f"'fear': {result['fear']}, "
+        f"'joy': {result['joy']} and "
+        f"'sadness': {result['sadness']}. "
+        f"The dominant emotion is {result['dominant_emotion']}."
+    )
+
+    return response_str
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
+
+    
